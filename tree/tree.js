eb8b711fa0adbd737714a1fc45b7a2c1fbfc8ca2
Bliss\Blissful <d.a.blissful@gmail.com>
2016-08-20 14:24:40 +0300

    удалил каменты и закомиченное

--- tree/tree.js
+++ tree/tree.js
@@ -13,34 +13,12 @@ function toggle(el) {
 }
 
 function hideEl(e) {
-    // // Узнать по какому элементу клик? e.target
-    // // Если клик не по "li", то найти родительский "li"
-    //
-    // if (e.target.tagName === 'LI') {
-    //     var clickEl = e.target;
-    // } else {
-    //     clickEl = e.target.closest('LI');
-    // }
-    // //todo Узнать есть ли дети?
-    //
-    // var hideClickEl = clickEl.children
-    //
-    // // if( hideClickEl.indexOf('LI') ) {
-    // //
-    // // }
-    //
-    // // toggle(clickEl);
-    // console.log(e);
-    // console.log(clickEl);
-    if( e.target.hasAttributes('hide') ) {
-        for( var i = 0; e.target.children.length > i; i++ ) {
+    if (e.target.hasAttributes('hide')) {
+        for (var i = 0; e.target.children.length > i; i++) {
             var forEl = e.target.children[i];
             console.log(i, forEl);
         }
-            toggle(forEl);
-        // Выбрать из елемента на котором произошло событие, елементы у которых
-        // Есть children_ы
-        console.log(e);
+        toggle(forEl);
     }
 
 }
